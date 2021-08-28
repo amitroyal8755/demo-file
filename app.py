508643540503a8dcb53a9d1becba33dc88136c30
Amit Kumar <86214957+amitroyal8755@users.noreply.github.com>
2021-08-28 11:08:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
-import os
-from pml import app
-port = int(os.environ.get('PORT', 5000))
+
+
 app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
 
@@ -26,4 +25,4 @@ def predict():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=port,debug=True)
+    app.run(debug=True)
